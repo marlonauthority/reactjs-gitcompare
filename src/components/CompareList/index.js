666271e8da9c8bb5f5be69675c320c94d72df115
marlondck <15050029+marlondck@users.noreply.github.com>
2019-04-06 17:03:51 -0400

    component

---
+++ src/components/CompareList/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Container, Repository } from './styles';
+
+const CompareList = () => (
+  <Container>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          10,000
+          <small>forks</small>
+        </li>
+        <li>
+          9
+          <small>issues</small>
+        </li>
+        <li>
+          3 days
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          10,000
+          <small>forks</small>
+        </li>
+        <li>
+          9
+          <small>issues</small>
+        </li>
+        <li>
+          3 days
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          95,019
+          <small>stars</small>
+        </li>
+        <li>
+          10,000
+          <small>forks</small>
+        </li>
+        <li>
+          9
+          <small>issues</small>
+        </li>
+        <li>
+          3 days
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+  </Container>
+);
+
+export default CompareList;
